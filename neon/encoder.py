249531901aeb504fb66b16f24777aabd642c705b
Pavel Dedík <pavel.dedik@threatmark.com>
2021-07-13 10:08:40 +0200

    Add pre-commit and github actions, drop support for python 3.5

--- neon/encoder.py
+++ neon/encoder.py
@@ -5,21 +5,22 @@ from __future__ import unicode_literals
 
 
 def format_list(list_, indent_level):
-    indent = '\t' * indent_level
-    newline = '\n' if indent_level else '\n\n'
-    return newline.join([
-        '{}- {}'.format(indent, to_string(value, indent_level + 1))
-        for value in list_
-    ])
+    indent = "\t" * indent_level
+    newline = "\n" if indent_level else "\n\n"
+    return newline.join(
+        ["{}- {}".format(indent, to_string(value, indent_level + 1)) for value in list_]
+    )
 
 
 def format_dict(dict_, indent_level):
-    indent = '\t' * indent_level
-    newline = '\n' if indent_level else '\n\n'
-    return newline.join([
-        '{}{}: {}'.format(indent, key, to_string(value, indent_level + 1))
-        for key, value in dict_.items()
-    ])
+    indent = "\t" * indent_level
+    newline = "\n" if indent_level else "\n\n"
+    return newline.join(
+        [
+            "{}{}: {}".format(indent, key, to_string(value, indent_level + 1))
+            for key, value in dict_.items()
+        ]
+    )
 
 
 def to_string(obj, indent_level=0):
@@ -30,9 +31,9 @@ def to_string(obj, indent_level=0):
     :rtype: string
     """
     if isinstance(obj, dict):
-        return '\n' + format_dict(obj, indent_level)
+        return "\n" + format_dict(obj, indent_level)
     elif isinstance(obj, list):
-        return '\n' + format_list(obj, indent_level)
+        return "\n" + format_list(obj, indent_level)
     elif obj is None:
-        return 'Null'
+        return "Null"
     return str(obj)
